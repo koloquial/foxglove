63ee88c41fd12e7f8565f5b781a285a2539184af
Nick Kolodziej <nick@koloquial.cc>
2022-07-08 14:52:47 -0500

    added store to dash

--- src/screens/Dashboard/Dashboard.js
+++ src/screens/Dashboard/Dashboard.js
@@ -1,32 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
 //Authentication
-import { useAuth } from '../../authentication/AuthContext'
+import { useAuth } from '../../authentication/AuthContext';
 
-//Navigation
-import { useNavigate } from 'react-router-dom'
+//Components
+import Loading from '../../components/Loading';
+
+//State
+import { useSelector, useDispatch } from 'react-redux';
+
+//Functions
+import { update } from '../../functions/update';
 
 const Dashboard = () => {
+  //State
+  const account = useSelector(state => state.account.database)
+  const loading = useSelector(state => state.account.loading)
 
-  const { currentUser, logout } = useAuth();
-  const navigate = useNavigate();
+  const { currentUser } = useAuth()
 
-  const handleLogout = async () => {
-    try {
-      await logout()
-      navigate('/splash')
-    } catch (error) {
-
+  useEffect(() => {
+    if (currentUser.uid) {
+      update(currentUser.uid)
     }
-  }
+  }, [])
 
   return (
     <div>
-      Dashboard
-      {console.log('current user:', currentUser)}
-      <button onClick={handleLogout}>signout</button>
+      <Navigation />
+      <br />
+
+      {loading ?
+        <Loading /> :
+        <div className='fade'>
+          <center>
+            <h3 className='darkGreen'>Dashboard</h3>
+            <br /><br />
+          </center>
+
+          {action.createHoa ? <CreateHOA /> : <></>}
+
+          {action.createSheet && !action.createHoa ? <CreateSheet /> : <></>}
+
+          {Object.keys(view).length && !action.createHoa ? <Glance /> : <></>}
+        </div >
+      }
     </div>
   )
 }
 
-export default Dashboard;
+export default Dashboard
